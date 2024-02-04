2770aede0b425c5111cca5865a6ad23899b768ee
vladimir_skrypnikov <sevgrad@list.ru>
2024-02-04 15:32:24 +0300

    Homework 1

---
+++ HomeWork_1/app/src/main/java/com/example/passengercounter/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.passengercounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.passengercounter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var counter: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonPlus.setOnClickListener {
+            checkStatus(binding)
+            counter++
+            checkStatus(binding)
+        }
+
+        binding.buttonMinus.setOnClickListener {
+            checkStatus(binding)
+            counter--
+            checkStatus(binding)
+        }
+
+        binding.buttonReset.setOnClickListener {
+            startCondition(binding)
+        }
+    }
+
+    private fun checkStatus(binding: ActivityMainBinding) {
+        when (counter) {
+            0 -> startCondition(binding)
+            in 1..49 -> workCondition(binding)
+            50 -> finishCondition(binding)
+        }
+    }
+
+    private fun startCondition(binding: ActivityMainBinding) {
+        counter = 0
+        binding.buttonMinus.isEnabled = false
+        binding.buttonPlus.isEnabled = true
+        binding.buttonReset.visibility = View.INVISIBLE
+        binding.textViewStatus.text = getText(R.string.seats_free)
+        binding.textViewStatus.setTextColor(getColor(R.color.green))
+        binding.textViewPassengerCounter.text = counter.toString()
+    }
+
+    private fun workCondition(binding: ActivityMainBinding) {
+        binding.buttonMinus.isEnabled = true
+        binding.buttonPlus.isEnabled = true
+        binding.buttonReset.visibility = View.INVISIBLE
+        binding.textViewStatus.setTextColor(getColor(R.color.purple))
+        binding.textViewPassengerCounter.text = counter.toString()
+        val outputText = "${getText(R.string.seats_left)} $counter"
+        binding.textViewStatus.text = outputText
+    }
+
+    private fun finishCondition(binding: ActivityMainBinding) {
+        binding.buttonMinus.isEnabled = false
+        binding.buttonPlus.isEnabled = false
+        binding.buttonReset.visibility = View.VISIBLE
+        binding.textViewStatus.text = getText(R.string.no_seats)
+        binding.textViewStatus.setTextColor(getColor(R.color.red))
+        binding.textViewPassengerCounter.text = counter.toString()
+    }
+}
